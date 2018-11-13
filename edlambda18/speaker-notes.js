2c55fed7d65d978ebaf1656fe8d0646bc4ed49db
Sam Halliday <sam.halliday@gmail.com>
2018-11-13 22:09:38 +0000

    skeleton for EdLambda talk

---
+++ edlambda18/speaker-notes.js
@@ -0,0 +1,17 @@
+  <script src="http://code.jquery.com/jquery-1.11.1.min.js" type="text/javascript"></script>
+  <script type="text/javascript">
+    $("div").hide();
+    $("ul").hide();
+    $("ol").hide();
+    $("span").hide();
+    $("pre").hide();
+    $("code").hide();
+    $("table").hide();
+    $("video").hide();
+    $("img").hide();
+    $("a").hide();
+    $("p").hide();
+
+    $(".notes").show();
+    $(".notes").find("*").show();
+  </script>
